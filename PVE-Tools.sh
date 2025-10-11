599a4d661cfc8419c1b0bff767afc28f541cdc29
Introl-ljl <3226749226@qq.com>
2025-10-11 08:02:49 +0000

    Fix: 修改菜单选择输入方式，使用read命令避免重复。

--- PVE-Tools.sh
+++ PVE-Tools.sh
@@ -2437,7 +2437,7 @@ main() {
     while true; do
         show_banner
         show_menu
-        choice=$(get_menu_choice "${GREEN}请输入您的选择 [0-16, 520]: ${NC}" "0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 520" "0")
+        read -r choice
         echo
         echo
         
